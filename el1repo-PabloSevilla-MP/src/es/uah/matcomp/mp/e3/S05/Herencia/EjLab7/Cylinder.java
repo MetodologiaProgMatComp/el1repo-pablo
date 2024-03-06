36248df3839b6da7166930dfc5df1cf1ef4f8ba7
psevilla05 <pablosevillamendez@gmail.com>
2024-03-06 09:25:47 +0100

    p2 ej7

--- el1repo-PabloSevilla-MP/src/es/uah/matcomp/mp/e3/S05/Herencia/EjLab7/Cylinder.java
+++ el1repo-PabloSevilla-MP/src/es/uah/matcomp/mp/e3/S05/Herencia/EjLab7/Cylinder.java
@@ -1,10 +1,42 @@
 package es.uah.matcomp.mp.e3.S05.Herencia.EjLab7;
 
-public class Cylinder {
+public class Cylinder extends Circle {
     Circle base;
-    double height;
+    private double height;
+    // Constructor with default color, radius and height
     public Cylinder() {
-        base = new Circle(); // Call the constructor to construct the Circle
+        super(); // call superclass no-arg constructor Circle()
         height = 1.0;
+    }
+    // Constructor with default radius, color but given height
+    public Cylinder(double height) {
+        super(); // call superclass no-arg constructor Circle()
+        this.height = height;
+    }
+    // Constructor with default color, but given radius, height
+    public Cylinder(double radius, double height) {
+        super(radius); // call superclass constructor Circle(r)
+        this.height = height;
+    }
+
+    // A public method for retrieving the height
+    public double getHeight() {
+        return height;
+    }
+
+
+    // A public method for computing the volume of cylinder
+    // use superclass method getArea() to get the base area
+    public double getVolume() {
+        return super.getArea()*height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder[" +
+                "radius=" + getRadius() +
+                ", color='" + getColor() + '\'' +
+                ", height=" + height +
+                ']';
     }
 }
